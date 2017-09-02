624818f41c02729c8477a405f6373227e41b3836
Marcus Fihlon <marcus@fihlon.ch>
2017-09-02 19:51:09 +0200

    :bug: Forgot to commit the HttpModule declaration in app module #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule} from "@angular/http";
 
 import { AppComponent } from './components/app.component';
 import { EventService} from './services/event.service';
@@ -12,11 +13,12 @@ import { TimePipe } from './pipes/time.pipe';
     AppComponent
   ],
   imports: [
-    BrowserModule
   ],
   bootstrap: [AppComponent]
         DatePipe,
         TimePipe
+        BrowserModule,
+        HttpModule
     providers: [EventService],
 })
 export class AppModule { }
